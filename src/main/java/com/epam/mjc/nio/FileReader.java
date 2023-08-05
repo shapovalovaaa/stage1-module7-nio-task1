8e4f3e715c71456c6b373f4100fc72e2d352d19c
shapovalovaaa <nastyashpovalova70@gmail.com>
2023-08-05 15:09:36 +0300

    feat: solve task

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,38 +1,53 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        String content;
-        String[] res;
-
-        try (FileInputStream fileInputStream = new FileInputStream(file.getPath())) {
-            byte[] bytes = Files.readAllBytes(Paths.get(file.getPath()));
-            content = new String(bytes);
-            res = finalContent(content);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return new Profile(res[0], Integer.parseInt(res[1]), res[2], Long.parseLong(res[3]));
+    public static Profile getDataFromFile(File file) {
+        String fileData = readFileToString(file);
+        Map<String, String> profileData = parseData(fileData);
+        return createProfile(profileData);
     }
 
-    public static String[] finalContent(String content) {
-        String[] finalContent = new String[4];
-        String[] lines = content.split("\\r?\\n");
-        for (int i = 0; i < lines.length; i++) {
-            String[] parts = lines[i].split(":");
-            if (parts.length == 2) {
-                finalContent[i] = parts[1].trim();
+    private static String readFileToString(File file) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = Files.newBufferedReader(file.toPath())) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+    private static Map<String, String> parseData(String data) {
+        Map<String, String> profileData = new HashMap<>();
+        String[] lines = data.split("\n");
+        for (String line : lines) {
+            String[] keyValue = line.split(": ");
+            if (keyValue.length == 2) {
+                String key = keyValue[0].trim();
+                String value = keyValue[1].trim();
+                profileData.put(key, value);
             }
         }
-        return finalContent;
+        return profileData;
+    }
+
+    private static Profile createProfile(Map<String, String> profileData) {
+        String name = profileData.get("Name");
+        int age = Integer.parseInt(profileData.get("Age"));
+        String email = profileData.get("Email");
+        String phone = profileData.get("Phone");
+        return new Profile(name, age, email, Long.valueOf(phone));
     }
 }
