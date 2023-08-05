2f256dd75fedd11c5043aa3951997a3bf7ddb278
shapovalovaaa <nastyashpovalova70@gmail.com>
2023-08-05 15:05:53 +0300

    feat: solve task

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,38 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String content;
+        String[] res;
+
+        try (FileInputStream fileInputStream = new FileInputStream(file.getPath())) {
+            byte[] bytes = Files.readAllBytes(Paths.get(file.getPath()));
+            content = new String(bytes);
+            res = finalContent(content);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Profile(res[0], Integer.parseInt(res[1]), res[2], Long.parseLong(res[3]));
+    }
+
+    public static String[] finalContent(String content) {
+        String[] finalContent = new String[4];
+        String[] lines = content.split("\\r?\\n");
+        for (int i = 0; i < lines.length; i++) {
+            String[] parts = lines[i].split(":");
+            if (parts.length == 2) {
+                finalContent[i] = parts[1].trim();
+            }
+        }
+        return finalContent;
     }
 }
